100835471e5f0ddeaac64706696563d3b55664f1
EC2 Default User <ec2-user@ip-172-31-58-19.ec2.internal>
2021-06-07 14:50:22 +0000

    フォロー機能実装中

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users, only: [:show,:index,:edit,:update]
+
+  resources :users, only: [:index, :show, :edit, :update] do
+  resources :relationships, only: [:create, :destroy]
+  get 'followings' => 'relationships#followings', as: 'followings'
+  get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :books do
-   resource :favorites, only: [:create, :destroy] 
+   resource :favorites, only: [:create, :destroy]
   resources :book_comments, only: [:create, :destroy]
-   
+
   end
 end
